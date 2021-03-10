d2edbf95291f451030f53c359de3aaf4d4b19b5d
Florian Rodriguez <florian@negative-network.com>
2021-03-10 10:44:14 +0100

    Fix create article form

--- Application/Views/backend/article/article-form.php
+++ Application/Views/backend/article/article-form.php
@@ -3,15 +3,15 @@
   <div class="row column text-center">
     <h2 class="subheader">Nouvel Article</h2>
     <form action="?admin=true&type=article&action=create" method="post">
-      <input type="text" name="post_title" placeholder="Titre">
-      <input type="text" name="post_category" placeholder="Category">
-      <textarea cols="30" id="content" name="post_content" rows="10">Ici le contenu de votre article</textarea>
+      <input type="text" name="post_title" value="" placeholder="Titre">
+      <input type="text" name="post_category" value="" placeholder="Category">
+      <textarea cols="30" id="content" value="" name="post_content" rows="10">Ici le contenu de votre article</textarea>
       <label for="status-select">Status:</label>
       <select name="post_status" id="status-select">
         <option value="">--Choisir une option--</option>
-        <option valeur="draft">Brouillon</option>
-        <option valeur="published">Publié</option>
-        <option valeur="trash">Poubelle</option>
+        <option value="draft">Brouillon</option>
+        <option value="published">Publié</option>
+        <option value="trash">Poubelle</option>
       </select>
       <input type="submit" value="Envoyer">
     </form>
